71cf4fe72b094c9cb990fdbbdf5b4db516677187
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2023-01-13 20:34:22 +0400

    add SQLite3

--- d_z_bd_aircraft.py
+++ d_z_bd_aircraft.py
@@ -1,6 +1,5 @@
 import sqlite3 as sq
 
-
 aircraft = [
     ('Би-Кок', 1914, 'А.А. Пороховщиков'),
     ('Четырёхплан Савельева', 1916, 'В.Ф. Савельев'),
@@ -25,28 +24,28 @@ with sq.connect("aircraft.db") as con:
         author TEXT
     )
     """)
-    # cur.execute("DELETE FROM aircraft WHERE avion_id LIKE '14'")
+    cur.execute("DELETE FROM aircraft WHERE avion_id LIKE '14'")
 
-    # cur.executescript("""
-    # DELETE FROM aircraft WHERE avion_id LIKE '15';
-    # DELETE FROM aircraft WHERE avion_id LIKE '16';
-    # INSERT INTO aircraft VALUES(NULL, 'У-2', 1928, 'Н.Н. Поликарпов');
-    # INSERT INTO aircraft VALUES(NULL, 'БИЧ-7', 1929, 'Б.И.Черановский')
-    # """)
+    cur.executescript("""
+    DELETE FROM aircraft WHERE avion_id LIKE '15';
+    DELETE FROM aircraft WHERE avion_id LIKE '16';
+    INSERT INTO aircraft VALUES(NULL, 'У-2', 1928, 'Н.Н. Поликарпов');
+    INSERT INTO aircraft VALUES(NULL, 'БИЧ-7', 1929, 'Б.И.Черановский')
+    """)
 
-    # cur.execute("UPDATE aircraft SET year = :Year WHERE model LIKE 'Св%'", {'Year': 1915})
+    cur.execute("UPDATE aircraft SET year = :Year WHERE model LIKE 'Св%'", {'Year': 1915})
 
-    # cur.executemany("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", models)
+    cur.executemany("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", models)
 
-    # for avion in models:
-    #     cur.execute("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", avion)
+    for avion in models:
+        cur.execute("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", avion)
 
-    # for aircr in aircraft:
-    #     cur.execute("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", aircr)
+    for aircr in aircraft:
+        cur.execute("INSERT INTO aircraft VALUES(NULL, ?, ?, ?)", aircr)
 
 
-    # cur.execute("INSERT INTO aircraft VALUES(1, 'Биплан Докучаева', 1910, 'А.Я. Докучаев')")
-    # cur.execute("INSERT INTO aircraft VALUES(2, 'Гаккель-VIII', 1912, 'Я.М. Гаккель')")
-    # cur.execute("INSERT INTO aircraft VALUES(3, 'С-6Б', 1912, 'И.И. Сикорский')")
-    # cur.execute("INSERT INTO aircraft VALUES(4, 'Русский Витязь', 1913, 'И.И. Сикорский')")
-    # cur.execute("INSERT INTO aircraft VALUES(5, 'Лебедь-12', 1915, 'В.А. Лебедев')")
+    cur.execute("INSERT INTO aircraft VALUES(1, 'Биплан Докучаева', 1910, 'А.Я. Докучаев')")
+    cur.execute("INSERT INTO aircraft VALUES(2, 'Гаккель-VIII', 1912, 'Я.М. Гаккель')")
+    cur.execute("INSERT INTO aircraft VALUES(3, 'С-6Б', 1912, 'И.И. Сикорский')")
+    cur.execute("INSERT INTO aircraft VALUES(4, 'Русский Витязь', 1913, 'И.И. Сикорский')")
+    cur.execute("INSERT INTO aircraft VALUES(5, 'Лебедь-12', 1915, 'В.А. Лебедев')")
